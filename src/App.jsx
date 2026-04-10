473cdf57dc04514f22739560921d341cfcc511a8
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-10 20:31:18 +0600

    add search functionality and styling to App component; update dependencies and remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { FaSearchLocation } from "react-icons/fa";
+import sun from "./assets/sun.png";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data.get("search"));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type="text" name="search" id="search" placeholder="City" />
+        <button type="submit">
+          <FaSearchLocation />
+        </button>
+      </form>
+      <img src={sun} alt="sun" />
+      <p>temperature</p>
+      <p>city</p>
+      <div>
+        <div>random value</div>
+        <div>random value</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
