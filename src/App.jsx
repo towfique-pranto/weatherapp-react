e4a58caa88a1251b843b8938a0d8da5f36de2de1
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-10 21:04:04 +0600

    replace App.css with App.module.css and add styles to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { FaSearchLocation } from "react-icons/fa";
+import { FaSearchLocation, FaWind } from "react-icons/fa";
+import { IoWater } from "react-icons/io5";
 import sun from "./assets/sun.png";
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
   const handleSearch = (e) => {
@@ -10,19 +11,31 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input type="text" name="search" id="search" placeholder="City" />
-        <button type="submit">
+    <div className={styles.container}>
+      <form onSubmit={handleSearch} className={styles.searchContainer}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="City"
+          className={styles.searchBar}
+        />
+        <button type="submit" className={styles.searchBtn}>
           <FaSearchLocation />
         </button>
       </form>
-      <img src={sun} alt="sun" />
-      <p>temperature</p>
-      <p>city</p>
-      <div>
-        <div>random value</div>
-        <div>random value</div>
+      <img className={styles.weatherIcon} src={sun} alt="sun" />
+      <p className={styles.temperature}>temperature</p>
+      <p className={styles.city}>city</p>
+      <div className={styles.extraInfo}>
+        <div className={styles.infoContainer}>
+          <IoWater className={styles.icon} />
+          <div>Humidity: 70%</div>
+        </div>
+        <div className={styles.infoContainer}>
+          <FaWind className={styles.icon} />
+          <div>Windspeed: 70%</div>
+        </div>
       </div>
     </div>
   );
